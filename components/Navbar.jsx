0c35f57052b24009b7164c7ebb034277ff5cf3fb
rbabaci1 <4rabah@gmail.com>
2023-03-09 12:17:59 -0800

    implement the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
+
+import { Cart } from './';
+// import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>JSM Headphones</Link>
+      </p>
+
+      <button
+        type='button'
+        className='cart-icon'
+        // onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>{2}</span>
+      </button>
+
+      {true && <Cart />}
+    </div>
+  );
 };
 
 export default Navbar;
